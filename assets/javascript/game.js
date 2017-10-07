bb0ff78d5c460085707c692148da7301512aa4d8
Kyle <k90armstrong@gmail.com>
2017-10-07 16:07:30 -0600

    adds the target and cleans some code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,16 @@
+// MAP FUCNTION HERE
 function myMap() {
-    var coords = getUserLocation();
-}
-
-function getUserLocation() {
-    var latitude = 40.9628845;
-    var longitude = -112.0953297;
-    var latLng = {
-        lat: latitude,
-        lng: longitude
+    var mapProp = {
+        zoom: 10,
+        disableDefaultUI: true
     };
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(changeLatLong);
-    } else {
-        var mapProp = {
-            center: new google.maps.LatLng(latitude, longitude), // get current location from browser, if can't default to washingtn dc
-            zoom: 10,
-            disableDefaultUI: true
-        };
-        map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-    }
+    map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    var latLng = getUserLocation();
+    var overlay = new google.maps.OverlayView();
+    overlay.draw = function () {};
+    overlay.setMap(map);
+    // targetPX = overlay.getProjection().fromLatLngToCntainerPixel(latLng);
+    // console.log(targetPX);
     marker = new google.maps.Marker({
         position: latLng,
         map: map,
@@ -26,31 +18,22 @@ function getUserLocation() {
     });
 }
 
-function changeLatLong(position) {
-    var latLng = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-    };
-    var mapProp = {
-        center: {
-            lat: latLng.lat - .1,
-            lng: latLng.lng
-        }, //new google.maps.LatLng(position.coords.latitude, position.coords.longitude), // get current location from browser, if can't default to washingtn dc
-        zoom: 10,
-        disableDefaultUI: true,
-        draggable: false
-    };
-    map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-    marker = new google.maps.Marker({
-        position: latLng,
-        map: map,
-        title: 'Target!'
-    });
-    var overlay = new google.maps.OverlayView();
-    overlay.draw = function () {};
-    overlay.setMap(map);
-    // targetPX = overlay.getProjection().fromLatLngToCntainerPixel(latLng);
-    // console.log(targetPX);
+function getUserLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            var latLng = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            changeLocation(latLng);
+        });
+    } else {
+        // if user doesnt allow for the location info just start with the first index in the location array
+        var loc = locations[game.locationIndex];
+        var latLng = loc.latLng;
+        game.locationIndex += 1;
+        changeLocation(latLng);
+    }
 }
 
 function changeLocation(latLng) {
@@ -59,32 +42,44 @@ function changeLocation(latLng) {
         lng: latLng.lng
     })
     marker.setPosition(latLng);
+    var glatLang = new google.maps.LatLng(latLng.lat, latLng.lng);
+    var test = map.getProjection().fromLatLngToPoint(glatLang);
+    console.log(test);
+    target.className = "fa fa-bullseye target";
+    target.style.bottom = test.y + 'px';
+    target.style.left = test.x + 'px';
+
+
 }
 
-
-
-
-
-
+// MAIN GAME HERE CALLBACK FOR KEYUP EVENT
 function mainGame(event) {
     var keyPressed = event.key;
     if (game.inString(keyPressed)) {
-        console.log('you guessed right');
         game.succesfulAttempt(keyPressed);
         var newString = game.getNewStrng();
         game.updateString(newString);
     } else {
-        console.log('you guessed wrong');
         game.failedAttempt(keyPressed);
     }
     game.checkStatus();
     game.updateBottomStats();
 }
 
+// functions to change the DOM based on a win or a loss
 function changeScreenToWin() {
     loginDiv.className = 'my-center display-none';
     document.getElementById('entire-password').className = 'display-none';
     messageP.innerHTML = 'You just saved the world from going into another world war! Congrats!';
+    messageP.className = '';
+    mainButton.style.visibility = 'hidden';
+}
+
+function changeScreenToLose() {
+    gif.className = "lose-gif";
+    loginDiv.className = 'my-center display-none';
+    document.getElementById('entire-password').className = 'display-none';
+    messageP.innerHTML = 'Few that was close! It looks like the missile didn\'t make it very far!';
     messageP.className = '';
     mainButton.style.visibility = 'hidden';
 }
@@ -95,18 +90,53 @@ function startNewGame() {
     mainButton.innerHTML = 'Start!'
 }
 
+// function to get the position of an element
+function getBottom(element) {
+    var rect = element.getBoundingClientRect();
+    return rect.bottom
+
+}
+
+// function to shuffle an array 
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+// Declare Variables Below Here
 var latitude;
 var longitude;
 var targetPX;
 var map;
 var marker;
+var computerElement = document.getElementById('computer');
+console.log('here is the bottom' + getBottom(computerElement));
+var bottom = getBottom(computerElement);
+var inner = document.getElementById('inner');
+inner.style.top = bottom + 'px';
 var gif = document.getElementById('gif');
+var target = document.getElementById('target');
 var usernameSpan = document.getElementById('username');
 var passwordSpan = document.getElementById('password');
 var messageP = document.getElementById('intro');
 var mainButton = document.getElementById('main-button');
 var loginDiv = document.getElementById('login');
-var usernames = ['kim', 'denisrodman'];
+var usernames = ['kim', 'dennisrodman'];
 var passwords = ['mouserat', 'appletree'];
 var locations = [{
         name: 'the White House',
@@ -149,6 +179,10 @@ var locations = [{
         }
     }
 ];
+
+locations = shuffle(locations);
+
+// missile object and the html element
 var missileElement = document.getElementById('missile');
 var missile = {
     moveMissileIntreval: null,
@@ -159,22 +193,16 @@ var missile = {
         // missile.style.position = "relative";
         var startX = rect.top;
         var startY = rect.right;
-        console.log('startx: ' + startX);
-        console.log('starty: ' + startY);
 
         var deltaX = finalX - startX;
         var deltaY = finalY - startY;
         var totalDistance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-        console.log(totalDistance);
         var fps = 10;
         var changePerMoveX = deltaX / (totalTime * fps);
         var changePerMoveY = deltaY / (totalTime * fps);
-        console.log('change per x ' + changePerMoveX);
 
 
         this.moveMissileIntreval = setInterval(function () {
-            console.log('Moving missile');
-            console.log(missileElement.style.top);
             var oldTop = missileElement.style.top.replace('px', '');
             var oldLeft = missileElement.style.left.replace('px', '');
             if (oldTop === '') {
@@ -187,7 +215,6 @@ var missile = {
             var newLeft = parseFloat(oldLeft) + changePerMoveX;
             missileElement.style.top = newTop + 'px';
             missileElement.style.left = newLeft + 'px';
-            console.log('oldtop ' + oldTop);
         }, 100);
 
     },
@@ -199,6 +226,7 @@ var missile = {
 
 }
 
+// game object
 var game = {
     username: null,
     password: null,
@@ -207,6 +235,7 @@ var game = {
     remainingTries: 15,
     currentStep: 'username',
     location: null,
+    locationIndex: 0,
 
     startGame: function () {
         this.password = passwords[Math.floor(Math.random() * passwords.length)];
@@ -275,11 +304,15 @@ var game = {
     },
     checkStatus: function () {
         if (this.remainingTries <= 0) {
-            console.log('game over');
+            // check if missile hit target TODO
+            changeScreenToLose();
+            missile.resetMissile();
+            game.changeLocation();
+            setTimeout(startNewGame, 3000);
+            return
         }
         var currentWord = this.getCurrentWordBeingGuessed();
         if (this.allGuessed()) {
-            console.log('all the way guessed');
             if (this.currentStep === 'username') {
                 this.currentStep = 'password';
                 this.correctGuesses = [];
@@ -292,10 +325,7 @@ var game = {
                 document.getElementById('main-button').style.visibility = 'visible';
                 document.getElementById('main-button').innerHTML = "Abort!";
             }
-        } else if (this.remainingTries === 0) {
-            gif.className = "lose-gif";
         }
-        // or check if time ran out
     },
     allGuessed: function () {
         var currentWord = this.getCurrentWordBeingGuessed();
@@ -314,15 +344,19 @@ var game = {
         }
     },
     changeLocation: function () {
-        this.location = locations[Math.floor(Math.random() * locations.length)];
+        this.location = locations[this.locationIndex];
+        this.locationIndex += 1;
+        if (this.locationIndex === locations.length) {
+            this.locationIndex = 0;
+            locations = shuffle(locations);
+        }
         changeLocation(this.location.latLng);
     }
 }
 
-
+// addind click event listener for the main button
 mainButton.addEventListener('click', function (event) {
     if (this.innerText === 'Start!') {
-        console.log('start the game');
         this.innerText = 'Abort!';
         messageP.className = "display-none";
         loginDiv.className = "my-center"; // making this visible
@@ -334,7 +368,6 @@ mainButton.addEventListener('click', function (event) {
         document.addEventListener('keyup', mainGame);
     } else if (this.innerText === 'Abort!') {
         // user won
-        console.log('user won!');
         // show user win screen and then play again after 3 seconds...
         changeScreenToWin();
         missile.resetMissile();
